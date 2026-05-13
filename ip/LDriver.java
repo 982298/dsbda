f821af52ac02ce327d1770e2159c135f57a6ae06
982298 <gamifiedlearnig.pvt.ltd@gmail.com>
2026-05-13 10:12:08 +0530

    Update LDriver.java

--- ip/LDriver.java
+++ ip/LDriver.java
@@ -26,3 +26,29 @@ public class LDriver {
 	System.exit(j.waitForCompletion(true)?0:1);
 	}
 }
+
+
+
+
+
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class LMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+
+	private final static IntWritable one = new IntWritable(1);
+
+	public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException
+	{
+		String line = value.toString();
+		String[] parts = line.split(" ");
+
+		String ip = parts[0];
+
+		con.write(new Text(ip), one);
+	}
+}
