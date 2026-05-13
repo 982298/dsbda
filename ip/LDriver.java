ef02ebaabfbcc85c42b47ce7d8abe0802bb7b8cf
982298 <gamifiedlearnig.pvt.ltd@gmail.com>
2026-05-13 10:12:32 +0530

    Update LDriver.java

--- ip/LDriver.java
+++ ip/LDriver.java
@@ -52,3 +52,37 @@ public class LMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
 		con.write(new Text(ip), one);
 	}
 }
+
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class LReducer extends Reducer<Text, IntWritable, Text, IntWritable>
+{
+	private Text maxword = new Text();
+	private int maxsum = 0;
+
+	public void reduce(Text word, Iterable<IntWritable> values, Context con) throws IOException, InterruptedException
+	{
+		int sum = 0;
+
+		for(IntWritable value : values)
+		{
+			sum += value.get();
+		}
+
+		// check for maximum
+		if(sum > maxsum){
+			maxsum = sum;
+			maxword.set(word);
+		}
+	}
+
+	// called once after all reduce calls
+	protected void cleanup(Context con) throws IOException, InterruptedException
+	{
+		con.write(maxword, new IntWritable(maxsum));
+	}
+}
