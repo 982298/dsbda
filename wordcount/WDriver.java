45afeb7f5214d831838df44d5f53ae22ad7b2cb9
982298 <gamifiedlearnig.pvt.ltd@gmail.com>
2026-05-13 10:14:00 +0530

    Update WDriver.java

--- wordcount/WDriver.java
+++ wordcount/WDriver.java
@@ -26,3 +26,50 @@ FileOutputFormat.setOutputPath(j, output);
 System.exit(j.waitForCompletion(true)?0:1);
 }
 }
+
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WMapper extends Mapper<LongWritable , Text, Text , IntWritable>{
+	
+	public void map(LongWritable key, Text value, Context con) throws IOException, InterruptedException {
+		
+		String line = value.toString();
+		String[] Words = line.split(",");
+		
+		for(String word : Words) {
+			
+			Text outputKey = new Text(word.toUpperCase().trim());
+			IntWritable outputValue = new IntWritable(1);
+			
+			con.write(outputKey, outputValue);
+		}
+	}
+}
+
+
+
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WReducer extends Reducer<Text, IntWritable, Text, IntWritable>{
+	
+	public void reduce(Text word, Iterable<IntWritable> values ,Context con) throws IOException, InterruptedException {
+		
+		int sum = 0;
+		
+		for(IntWritable value : values) {
+			
+			sum += value.get();
+		}
+		
+		con.write(word, new IntWritable(sum));
+	}
+}
